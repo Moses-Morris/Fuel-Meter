9c0c986495ef7aaa07a3ccc2f8a487e89a272f2b
Dan Uluma <danuluma@gmail.com>
2023-10-27 00:22:47 +0300

    Basics of the frontend and necessary dependancies

---
+++ fuel-meter-frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import './App.css';
+import FuelForm from './FuelForm'
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Fuel Meter App</h1>
+                <FuelForm />
+            </header>
+            
+        </div>
+    );
+}
+
+export default App;
---
+++ fuel-meter-frontend/src/index.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+//import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//serviceWorker.unregister();
