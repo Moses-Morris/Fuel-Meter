6c4d2dfc1b0d9c33ce6f915a495c6cd1843efe6e
owenmiano <owenmiash90@gmail.com>
2023-10-27 14:48:10 +0300

    add PUT and DELETE request functionality

--- routes/fuel.js
+++ routes/fuel.js
@@ -24,7 +24,38 @@ try {
     res.status(500).json({ error: 'Internal server error' });
 }
 });
+// Update fuel entry
+router.put('/update/:id', async (req, res) => {
+    try {
+        const entry = await FuelEntry.findOne({
+            where: {
+              _id: req.params.id,
+            },
+          });
+          if (entry) {
+            // Update the entry with data from req.body
+            await entry.update(req.body);         
+            return res.status(200).json({message:`Entry has been updated successfully`})
+          }
+            return res.status(404).json({ message: "Entry not found" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+    });
 
-// Implement PUT and DELETE
+// Delete fuel entry
+router.delete('/delete/:id', async (req, res) => {
+    try {
+        const removedEntry = await FuelEntry.findByIdAndDelete({_id:req.params.id})
+          if (removedEntry) {     
+            return res.status(204).json({message:`Entry has been deleted successfully`})
+          }
+        return res.status(404).json({ message: "Entry not found" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+    });
 
 module.exports = router;
