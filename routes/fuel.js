1681b834274453488a75d74ea5d33dff294b7096
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-10-29 17:07:15 +0300

    added mpesa endpoint integration

--- routes/fuel.js
+++ routes/fuel.js
@@ -1,6 +1,40 @@
 const express = require("express");
 const router = express.Router();
 const FuelEntry = require("../models/fuel");
+const request = require("request");
+
+const mpesaQueryEndpoint =
+  "https://sandbox.safaricom.co.ke/mpesa/transactionstatus/v1/query";
+
+// Endpoint to initiate M-Pesa payment
+router.post("/mpesa/payment", async (req, res) => {
+  try {
+    // Use the Safaricom API to initiate a payment request
+    // Send request to Safaricom's endpoint
+    // Handle the response from Safaricom
+
+    // Once the payment is initiated, return a response to the user
+    res.status(200).json({ message: "Payment request initiated successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Endpoint to receive payment status callback
+router.post("/mpesa/callback", async (req, res) => {
+  try {
+    // Safaricom will send payment status updates to this endpoint
+    // Verify and process the payment status information from Safaricom
+    // Update your database with the payment status
+
+    // Return a response to Safaricom to acknowledge receipt of the callback
+    res.status(200).send("Callback received");
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 router.get("/", async (req, res) => {
   try {
