062d6704013b1596f7c6b096ccd267e5fc2a40fa
Moses-Morris <mosesmorrisdev@gmail.com>
2023-10-30 10:33:37 +0300

    Link Express Server with React App.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("./db");
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
